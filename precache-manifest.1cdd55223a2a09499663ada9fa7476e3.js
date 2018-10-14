6894ddcdcc963384e3af2244650851513b9d484e
h4nds0m3j4ck <estrella.terenzio@gmail.com>
2018-10-13 22:48:41 -0400

    Updates

---
+++ precache-manifest.1cdd55223a2a09499663ada9fa7476e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/h4nds0m3j4ck/nytreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8bd39c73afaa241867f",
+    "url": "/h4nds0m3j4ck/nytreact/static/js/runtime~main.c8bd39c7.js"
+  },
+  {
+    "revision": "dcbcf25d88f3aa7c57e2",
+    "url": "/h4nds0m3j4ck/nytreact/static/js/main.dcbcf25d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/h4nds0m3j4ck/nytreact/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "dcbcf25d88f3aa7c57e2",
+    "url": "/h4nds0m3j4ck/nytreact/static/css/main.8a4c6d8b.chunk.css"
+  },
+  {
+    "revision": "e03f7bb0b7be6a4e3e01d5e4598b5b97",
+    "url": "/h4nds0m3j4ck/nytreact/index.html"
+  }
+];
